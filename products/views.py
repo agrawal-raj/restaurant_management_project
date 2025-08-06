ac9de6191921bc8bf9e6c83fe5ccb5c6839b7416
Raj Agrawal <agrawalraj.py@gmail.com>
2025-08-07 00:55:49 +0530

    20719

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import api_view
 
 from .models import Item
 from .serializers import ItemSerializer
@@ -24,3 +25,13 @@ class ItemView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def get_menu(request):
+    menu = [
+        {"name": "Margherita Pizza", "description": "Classic cheese and tomato pizza": "price": 8.99},
+        {"name": "Veggie Burger", "description" : "Grilled vegitable patty with lettuce and tomato", "price": 7.49},
+        {"name": "Pasta Alfredo", "description" : "Creamy pasta with parmesan cheese", "price": 9.99}
+    ]
+    return Response(menu)
+    
