eb479d8c698347ed3e101cb97448c0e305f68416
Raj Agrawal <agrawalraj.py@gmail.com>
2025-08-26 21:32:23 +0530

    38012

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', home, name='home'),
-    path('about/', TemplateView.as_view(template_name="about.html"), name='about'),    "))
+    path('about/', TemplateView.as_view(template_name="about.html"), name='about'),
 ]
